e8180f591c7015a1d87a6aa93b8e1450f9cfbfda
{hareesh} <{amarahareesh@1234}>
2026-02-27 20:52:32 +0530

    first commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.hareesh.todolist',
+  appName: 'TodoMobile',
+  webDir: 'build'
+};
+
+export default config;
